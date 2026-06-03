d9bd4bc2cf38157c6910b7fd283f1689193bb112
Sean Macfoy <sean_macfoy@outlook.com>
2026-06-03 20:50:06 +0100

    Implemented ball motion

--- src/whack_a_mole/constants.py
+++ src/whack_a_mole/constants.py
@@ -1,6 +1,9 @@
 import pygame as pg
 
 
-WIN_WIDTH = 640
-WIN_HEIGHT = 480
+WIN_WIDTH: int = 640
+WIN_HEIGHT: int = 480
 WIN_SIZE = pg.math.Vector2(WIN_WIDTH, WIN_HEIGHT)
+
+FRAME_RATE_FPS: int = 60
+MILLIS_PER_SEC: float = 1000.0
--- src/whack_a_mole/game.py
+++ src/whack_a_mole/game.py
@@ -6,11 +6,14 @@ class Game:
     def __init__(self):
         self.running = False
 
-
-    def init(self):
         pg.init()
         self.windowSurface = pg.display.set_mode(WIN_SIZE)
+        self.clock = pg.time.Clock()
         self.running = True
+
+        self.ballPosition = pg.math.Vector2(WIN_WIDTH/2.0, WIN_HEIGHT/2.0)
+        self.ballDirection = pg.math.Vector2()
+        self.ballSpeed: float = 400.0
 
 
     def quit(self):
@@ -26,25 +29,38 @@ class Game:
             if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 self.running = False
 
+        pressedKeys = pg.key.get_pressed()
 
-    def update(self):
-        pass
+        self.ballDirection.x = pressedKeys[pg.K_d] - pressedKeys[pg.K_a]
+        self.ballDirection.y = pressedKeys[pg.K_s] - pressedKeys[pg.K_w]
+
+        if self.ballDirection != pg.math.Vector2():
+            self.ballDirection.normalize_ip()
+
+        print(self.ballDirection)
+
+
+    def update(self, dt: float):
+        self.ballPosition += self.ballDirection * self.ballSpeed * dt
 
 
     def render(self):
         self.windowSurface.fill("#6495ED")
 
-        pg.draw.circle(self.windowSurface, "#FF0000", (WIN_WIDTH/2.0, WIN_HEIGHT/2.0), 32)
+        pg.draw.circle(self.windowSurface, "#FF0000", self.ballPosition, 32)
 
         pg.display.update()
 
 
-    def run(self):
-        self.init()
-
+    def loop(self):
         while self.running:
+            dt = self.clock.tick(FRAME_RATE_FPS) / MILLIS_PER_SEC
+
             self.handleEvents()
-            self.update()
+            self.update(dt)
             self.render()
 
+
+    def run(self):
+        self.loop()
         self.quit()
